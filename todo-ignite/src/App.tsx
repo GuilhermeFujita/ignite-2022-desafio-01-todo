7dbf3052f1d49ebce967fc536f80a02a32c8c166
GuilhermeFujita <fujitaguilherme@gmail.com>
2022-06-07 21:56:13 -0300

    Added header and logo

--- todo-ignite/src/App.tsx
+++ todo-ignite/src/App.tsx
@@ -1,5 +1,15 @@
+import { Header } from './components/Header';
+
+import styles from './/App.module.css';
 import './global.css';
 
 export function App() {
-  return <div className='App'>Hello World</div>;
+  return (
+    <div className='App'>
+      <div className={styles.header}>
+        <Header />
+      </div>
+      <div className={styles.wrapper}></div>
+    </div>
+  );
 }
